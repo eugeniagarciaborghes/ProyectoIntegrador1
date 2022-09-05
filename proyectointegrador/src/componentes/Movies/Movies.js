1b4b783632323a1f53081f4c134643fbe4d274ba
eugeniagarciaborghes <egarciaborghes@udesa.edu.ar>
2022-09-05 11:19:32 -0300

    mas de componentes

---
+++ proyectointegrador/src/componentes/Movies/Movies.js
@@ -0,0 +1,17 @@
+import React, {Component}  from "react";
+import Card from "../Card/Card";
+import TopBar from "../Topbar/TopBar";
+import "./Movies.css"
+
+class Movies extends Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            datos: [],
+            
+        }
+    }
+
+}
+
+export default Movies;
---
+++ proyectointegrador/src/componentes/Topbar/Topbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./TopBar.css";
+
+class TopBar extends Component{
+  constructor(props){
+    super(props)
+    this.state ={
+        valor:''
+
+    }
+}
+
+}
