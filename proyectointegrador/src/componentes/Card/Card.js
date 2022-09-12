63b9fc6a610b21aa6442f88204f258814d63d5d1
francosanticchia1 <franco.santicchia03@gmail.com>
2022-09-12 11:56:24 -0300

    home basico

--- proyectointegrador/src/componentes/Card/Card.js
+++ proyectointegrador/src/componentes/Card/Card.js
@@ -15,7 +15,7 @@ class Card extends Component {
            <article className="data-detail">
                 <div className="card-content">
                     <h4>{this.props.info.title}</h4>
-                    <p>{this.props.info.price}</p>
+                    <img src= {this.props.info.poster_path} alt= {this.props.info.title}></img>
                 </div>
                 <i className={`fas fa-${this.props.info.icon} fa-2x text-gray-300`}></i>
             </article>
