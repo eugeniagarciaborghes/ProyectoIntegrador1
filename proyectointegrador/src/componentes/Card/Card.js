7a50f95987dd19890f4fd6a775fef98175408cce
francosanticchia1 <franco.santicchia03@gmail.com>
2022-09-12 11:40:38 -0300

    levanta el proyecto

--- proyectointegrador/src/componentes/Card/Card.js
+++ proyectointegrador/src/componentes/Card/Card.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react'
-    /*import "./Card.css";*/
+/*import "./Card.css";*/
 
-    class Card extends Component {
-        constructor(props){
-            super(props)
-        }
-
+class Card extends Component {
+    constructor(props){
+        super(props)
+    }
         
     render () {
-        console.log(props.info)
+        console.log(this.props.info);
         return (
-            <div>{this.props.info}</div>
+           
+           //<div>{this.props.info}</div>
+
+           <article className="data-detail">
+                <div className="card-content">
+                    <h4>{this.props.info.title}</h4>
+                    <p>{this.props.info.price}</p>
+                </div>
+                <i className={`fas fa-${this.props.info.icon} fa-2x text-gray-300`}></i>
+            </article>
         )
     }    
 }
